b35bfc9ccb1b482a6bdd3ade2979c0adc0791902
jesperrr <jesperrr>
2008-01-13 20:49:11 +0000

    L10N_LANG = :da or any other locale you want to set. Now possible to set in environment.rb

--- init.rb
+++ init.rb
@@ -1,16 +1,12 @@
-# Choose language file here 
-# ONLY choose one of the ones below:
+# set L10N_LANG constant in your environment.rb or here
+# 
+# Choose language file here. Only one language file can be selected
+L10N_LANG ||= :da
+
 #require 'lang_en' #default language to run the test cases
-require 'lang_da'
-#require 'lang_de'
-#require 'lang_es'
-#require 'lang_fi'
-#require 'lang_fr'
-#require 'lang_nl'
-#require 'lang_ko'
-#require 'lang_se'
-#require 'lang_chef'
-#require 'lang_pirate'
+require  "lang_" + L10N_LANG.to_s
+
+#require 'lang_de', 'lang_es', 'lang_fi', 'lang_fr', 'lang_nl', 'lang_ko', 'lang_se', 'lang_chef', 'lang_pirate'
 # You can Add your own localization file and add it here
 
 
